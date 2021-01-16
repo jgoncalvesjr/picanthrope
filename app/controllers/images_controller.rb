3732e4799c3f9e3c30e849bf39eca7237c60c07a
jgoncalvesjr <jgoncalvesjr@gmail.com>
2021-01-16 14:32:21 -0500

    Implement image viewing pagination

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,8 +1,17 @@
 class ImagesController < ApplicationController
   
+  PAGE_SIZE = 12
+
   def index
     authorize Image
-    @images = policy_scope(Image).all
+    @page_size = PAGE_SIZE
+    @page = params[:page].to_i
+    @page = 1 if @page == 0
+    offset = (@page * @page_size) - @page_size
+    scope = policy_scope(Image).all
+    @images = scope.offset(offset).limit(@page_size)
+    @total_count = scope.count
+    @total_pages = (@total_count / @page_size.to_f).ceil
   end
   
   def new
